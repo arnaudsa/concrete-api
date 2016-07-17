998cf17c45c4d68b61332dd121516c38800156f4
Arnaud Alves <arnaudsalves@gmail.com>
2016-07-16 23:13:26 -0300

    Criacao do projeto

---
+++ src/main/java/com/concrete/ConcreteApiApplication.java
@@ -0,0 +1,12 @@
+package com.concrete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcreteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcreteApiApplication.class, args);
+	}
+}
